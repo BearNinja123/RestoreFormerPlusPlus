81ef8df62c9596bacc06123a199742a09fb8adb6
Thai-Hoa Nguyen <tonanhhoaduc@gmail.com>
2024-01-10 13:55:57 -0500

    pragma unroll 8 slightly faster, reorganize gn testbench .py

--- custom_ops/custom_gn.py
+++ custom_ops/custom_gn.py
@@ -100,63 +100,67 @@ if __name__ == '__main__':
     bf16: 
     '''
     C = 256
-    DTYPE = torch.bfloat16
-    #x = torch.arange(C).reshape((1, C, 1, 1)).float().cuda().requires_grad_(True)
-    #torch.random.manual_seed(0)
+    DTYPE = torch.float
+    MODE = 'bench' # can be 'check', 'bench', default does both
 
-    x = torch.randn((2, C, 4, 4), dtype=DTYPE).cuda().requires_grad_(True).contiguous(memory_format=torch.channels_last) #* 100
-    gn1 = GN_NHWCRef(4, C).cuda().to(DTYPE)
-    gn2 = GN_NHWC(4, C).cuda().to(DTYPE)
+    if MODE != 'bench':
+        #x = torch.arange(C).reshape((1, C, 1, 1)).float().cuda().requires_grad_(True)
+        #torch.random.manual_seed(0)
 
-    #x = torch.randn((2, C, 4, 4), dtype=DTYPE).cuda().requires_grad_(True) * 100
-    #gn1 = nn.GroupNorm(4, C).cuda().to(DTYPE)
-    #gn2 = GN_NCHW(4, C).cuda().to(DTYPE)
+        x = torch.randn((2, C, 4, 4), dtype=DTYPE).cuda().requires_grad_(True).contiguous(memory_format=torch.channels_last) #* 100
+        gn1 = GN_NHWCRef(4, C).cuda().to(DTYPE)
+        gn2 = GN_NHWC(4, C).cuda().to(DTYPE)
 
-    with torch.no_grad():
-        w = torch.randn((C,))
-        b = torch.randn((C,))
-        gn1.weight.copy_(w)
-        gn1.bias.copy_(b)
-        gn2.weight.copy_(w)
-        gn2.bias.copy_(b)
-    g1 = gn1(x)
-    g2 = gn2(x)
-    print('FORWARD')
-    print('g1', g1.shape)
-    print((g1-g2).reshape((g1.numel(),)))
-    print('BACKWARD')
-    print('g1 sum wrt x')
-    g1_grad_wrt_x = torch.autograd.grad(g1.sum(), x, retain_graph=True)[0] #.reshape((x.numel(),))
-    g2_grad_wrt_x = torch.autograd.grad(g2.sum(), x, retain_graph=True)[0] #.reshape((x.numel(),))
-    print(g1_grad_wrt_x-g2_grad_wrt_x)
+        #x = torch.randn((2, C, 4, 4), dtype=DTYPE).cuda().requires_grad_(True) * 100
+        #gn1 = nn.GroupNorm(4, C).cuda().to(DTYPE)
+        #gn2 = GN_NCHW(4, C).cuda().to(DTYPE)
 
-    print('g1 sum wrt w')
-    g1_grad_wrt_w = torch.autograd.grad(g1.sum(), gn1.weight, retain_graph=True)[0].reshape((gn1.weight.numel(),))
-    g2_grad_wrt_w = torch.autograd.grad(g2.sum(), gn2.weight, retain_graph=True)[0].reshape((gn1.weight.numel(),))
-    print(g1_grad_wrt_w - g2_grad_wrt_w)
+        with torch.no_grad():
+            w = torch.randn((C,))
+            b = torch.randn((C,))
+            gn1.weight.copy_(w)
+            gn1.bias.copy_(b)
+            gn2.weight.copy_(w)
+            gn2.bias.copy_(b)
+        g1 = gn1(x)
+        g2 = gn2(x)
+        print('FORWARD')
+        print('g1', g1.shape)
+        print((g1-g2).reshape((g1.numel(),)))
+        print('BACKWARD')
+        print('g1 sum wrt x')
+        g1_grad_wrt_x = torch.autograd.grad(g1.sum(), x, retain_graph=True)[0] #.reshape((x.numel(),))
+        g2_grad_wrt_x = torch.autograd.grad(g2.sum(), x, retain_graph=True)[0] #.reshape((x.numel(),))
+        print(g1_grad_wrt_x-g2_grad_wrt_x)
 
-    print('g1 sum wrt b')
-    g1_grad_wrt_b = torch.autograd.grad(g1.sum(), gn1.bias, retain_graph=True)[0].reshape((gn1.bias.numel(),))
-    g2_grad_wrt_b = torch.autograd.grad(g2.sum(), gn2.bias, retain_graph=True)[0].reshape((gn2.bias.numel(),))
-    print(g1_grad_wrt_b - g2_grad_wrt_b)
+        print('g1 sum wrt w')
+        g1_grad_wrt_w = torch.autograd.grad(g1.sum(), gn1.weight, retain_graph=True)[0].reshape((gn1.weight.numel(),))
+        g2_grad_wrt_w = torch.autograd.grad(g2.sum(), gn2.weight, retain_graph=True)[0].reshape((gn1.weight.numel(),))
+        print(g1_grad_wrt_w - g2_grad_wrt_w)
 
-    x_nchw = torch.randn((32, C, 128, 128), dtype=DTYPE, device='cuda').requires_grad_(True)
-    x_nhwc = x_nchw.contiguous(memory_format=torch.channels_last).cuda().requires_grad_(True)
-    gn_args = (32, C)
-    BENCH = 'fwd' # can be 'fwd', 'bwd', anything else is fwd + bwd
-    for gn_class, gn_input, desc in (
-            (GN_NHWC, x_nhwc, 'GN NHWC (custom op)'),
-            #(GN_NCHW, x_nchw, 'GN NCHW'),
-            (nn.GroupNorm, x_nchw, 'nn GN NCHW'),
-            #(nn.GroupNorm, x_nhwc, 'nn GN NHWC'),
-            #(GN_NHWCRef, x_nhwc, 'GN NHWC (reference)'),
-            ):
-        print(desc, BENCH)
-        gn_layer = gn_class(*gn_args).cuda().to(DTYPE)
-        g = gn_layer(gn_input)
-        for i in tqdm(range(1000)):
-            if BENCH != 'bwd':
-                g = gn_layer(gn_input)
-            if BENCH != 'fwd':
-                torch.autograd.grad(g.sum(), gn_input, retain_graph=True)
-            torch.cuda.synchronize()
+        print('g1 sum wrt b')
+        g1_grad_wrt_b = torch.autograd.grad(g1.sum(), gn1.bias, retain_graph=True)[0].reshape((gn1.bias.numel(),))
+        g2_grad_wrt_b = torch.autograd.grad(g2.sum(), gn2.bias, retain_graph=True)[0].reshape((gn2.bias.numel(),))
+        print(g1_grad_wrt_b - g2_grad_wrt_b)
+
+    if MODE != 'check':
+        x_nchw = torch.randn((32, C, 128, 128), dtype=DTYPE, device='cuda').requires_grad_(True)
+        x_nhwc = x_nchw.contiguous(memory_format=torch.channels_last).cuda().requires_grad_(True)
+        gn_args = (32, C)
+        BENCH = 'both' # can be 'fwd', 'bwd', anything else is fwd + bwd
+        for gn_class, gn_input, desc in (
+                (GN_NHWC, x_nhwc, 'GN NHWC (custom op)'),
+                #(GN_NCHW, x_nchw, 'GN NCHW'),
+                #(nn.GroupNorm, x_nchw, 'nn GN NCHW'),
+                (nn.GroupNorm, x_nhwc, 'nn GN NHWC'),
+                #(GN_NHWCRef, x_nhwc, 'GN NHWC (reference)'),
+                ):
+            print(desc, BENCH)
+            gn_layer = gn_class(*gn_args).cuda().to(DTYPE)
+            g = gn_layer(gn_input)
+            for i in tqdm(range(100)):
+                if BENCH != 'bwd':
+                    g = gn_layer(gn_input)
+                if BENCH != 'fwd':
+                    torch.autograd.grad(g.sum(), gn_input, retain_graph=True)
+                torch.cuda.synchronize()
